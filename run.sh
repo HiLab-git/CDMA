92a450ec98edf6cfa22c03834b8d73e5161a8afa
Lanfeng Zhong <95599703+lanfz2000@users.noreply.github.com>
2024-03-27 15:25:10 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3
 python train_CDMA.py --gpu 0 --portion 5
 
 # Journal Version
-python train_CDMA.py --gpu 0 --portion 5
+python train_CDMA_Plus.py --gpu 0 --portion 5
